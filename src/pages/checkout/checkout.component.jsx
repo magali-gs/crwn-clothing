c6ca3f7084aa0886c54c3d3992cb9fadba80b71e
Magali Goncalves <magali.goncalvess@gmail.com>
2021-08-16 13:27:43 +0200

    orthographe correction

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -36,7 +36,7 @@ const CheckoutPage = ({ cartItems, total}) => (
             <span>TOTAL: ${total}</span>
         </div>
 		<div className='test-warning'>
-			*Please use the following test caredit card for payments*
+			*Please use the following test credit card for payments*
 			<br />
 			4242 4242 4242 4242 - Exp: 01/2022 - CVV: 123
 		</div>
