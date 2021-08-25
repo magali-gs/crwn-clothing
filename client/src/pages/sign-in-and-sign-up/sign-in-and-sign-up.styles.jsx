685f7597ff91dbb567b800ad9405337ebfcec587
Magali Goncalves <magali.goncalvess@gmail.com>
2021-08-25 16:55:40 +0200

    removing node_modules

--- client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.styles.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const SignInAndSignUpContainer = styled.div`
-	display: flex;
-	width: 850px;
-	justify-content: space-between;
-	margin: 30px auto;
-`;
